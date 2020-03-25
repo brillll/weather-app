67ec1a6f0eb403ff047aee61399e5841c71c0ae3
brillll <brillyuval@gmail.com>
2020-03-25 16:39:47 +0200

    added cross origin headers

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+    // Pass to next layer of middleware
+    next();
+  });
+
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 });
